528704e7132e8d161e1cf1fc93e7f96ef1b8c4b8
dineshsingh099 <ddddineshrawatddd@gmail.com>
2025-11-30 17:12:03 +0530

    mplement user register and login functionality

--- backend/user-services/src/app.js
+++ backend/user-services/src/app.js
@@ -8,6 +8,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-app.use('/users', userRoutes);
+app.use('/users',userRoutes);
 
 export default app;
